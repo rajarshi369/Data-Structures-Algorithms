6fb1b394aef7f783b45416dd3248d561e1912c89
rajarshi369 <rajarshi.biswas.369@gmail.com>
2020-05-05 20:29:28 +0530

    Add files via upload

---
+++ sieve.cpp
@@ -0,0 +1,29 @@
+// print all primes from 1 to LIMIT using sieve of Erastosthenes
+
+// print all primes from 1 to LIMIT using sieve of Erastosthenes
+
+#include <bits/stdc++.h>
+
+const int LIMIT = 1e6;
+static bool prime[LIMIT];
+
+void sieve() {
+    memset(prime, true, sizeof prime);
+    prime[0] = prime[1] = false;
+    for (int i = 4; i < LIMIT; i += 2)
+        prime[i] = false;
+    for (int i = 3; i < LIMIT; i += 2) {
+        if (prime[i]) {
+            for (int j = 2 * i; j < LIMIT; j += i)
+                prime[j] = false;
+        }
+    }
+}
+
+int main(){
+    sieve();
+    for(int i = 2; i < LIMIT; i++){
+        if(prime[i]) 
+            std :: cout << i << " ";
+    }
+}
