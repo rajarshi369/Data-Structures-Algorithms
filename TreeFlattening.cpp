93ceca597ef23b5eae2bd25ee9d01281ea215b50
rajarshi369 <rajarshi.biswas.369@gmail.com>
2020-05-06 22:18:51 +0530

    Initial commit

---
+++ TreeFlattening.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 1e5; //number of nodes
+
+static int FT[2 * N];
+static int S[N];
+static int F[N];
+static int timer = 0;
+
+vector<int> adj[N];
+
+void dfs(int node, int par){
+    S[node] = timer;
+    FT[timer++] = node;
+    for(auto i : adj[node]){
+        if(i != par)
+            dfs(i, node);
+    }
+    F[node] = timer++; 
+}
+
+int main(){
+    int n, m, x, y;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        cin >> x >> y;
+        adj[x - 1].push_back(y - 1), adj[y - 1].push_back(x - 1);
+    }
+    dfs(0, -1);
+    for(int i : FT)
+        cout << i << " ";
+}
